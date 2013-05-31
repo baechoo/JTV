65867d59d097ae0b6b0dbd6f27f7e550b5a70b09
baechoo <baechoo80@naver.com>
2013-05-31 10:15:18 +0900

    Init
    초기 프로젝트 커밋

---
+++ src/com/jei/jtv/IntroActivity.java
@@ -0,0 +1,31 @@
+package com.jei.jtv;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+
+public class IntroActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_intro);
+		initialize();
+	}
+
+	private void initialize(){
+		Handler handler = new Handler(){
+			@Override
+			public void handleMessage(Message msg) {
+				finish();
+				startActivity(new Intent(IntroActivity.this, MainActivity.class));
+			}
+		};
+		
+		handler.sendEmptyMessageDelayed(0, 2000);
+	}
+	
+
+}
